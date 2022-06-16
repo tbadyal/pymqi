07d847f2812bb601c2f03bbfecddba200acad597
Tushar Badyal <tushar.badyal1@gmail.com>
2022-06-16 12:18:18 +0530

    initial
    initial

---
+++ code/tests/test_queue_manager.py
@@ -0,0 +1,230 @@
+""" Tests for pymqi.QueueManager class.
+"""
+
+import os
+import unittest
+from uuid import uuid4
+
+import config  # noqa
+import utils  # noqa
+
+try:
+    from typing import List
+except ImportError:
+    pass
+
+from test_setup import Tests  # noqa
+from test_setup import main  # noqa
+
+import pymqi
+import pymqi.CMQC
+
+
+class TestQueueManager(Tests):
+
+    CHCKCLNT = None  # type: int
+
+    @classmethod
+    def setUpClass(cls):
+        """Initialize test environment."""
+        super(TestQueueManager, cls).setUpClass()
+
+        # Get CHCKCLNT value
+        attrs = []  # type: List[pymqi.MQOpts]
+        attrs.append(pymqi.CFIL(Parameter=pymqi.CMQCFC.MQIACF_Q_MGR_ATTRS,
+                                Values=[pymqi.CMQC.MQCA_CONN_AUTH]))
+        results = cls.pcf.MQCMD_INQUIRE_Q_MGR(attrs)
+        connAuthName = results[0][pymqi.CMQC.MQCA_CONN_AUTH]
+
+        attrs = []  # type: List[pymqi.MQOpts]
+
+        attrs.append(pymqi.CFST(Parameter=pymqi.CMQC.MQCA_AUTH_INFO_NAME,
+                                String=connAuthName))
+        attrs.append(pymqi.CFIN(Parameter=pymqi.CMQC.MQIA_AUTH_INFO_TYPE,
+                                Value=pymqi.CMQC.MQAIT_IDPW_OS))
+        attrs.append(pymqi.CFIL(Parameter=pymqi.CMQCFC.MQIACF_AUTH_INFO_ATTRS,
+                                Values=[pymqi.CMQC.MQIA_CHECK_CLIENT_BINDING]))
+
+        results = cls.pcf.MQCMD_INQUIRE_AUTH_INFO(attrs)
+        cls.CHCKCLNT = results[0][pymqi.CMQC.MQIA_CHECK_CLIENT_BINDING]
+
+        # Add required rights for pinging QMGR
+        attrs = []  # type: List[pymqi.MQOpts]
+        attrs.append(pymqi.CFST(Parameter=pymqi.CMQCFC.MQCACF_AUTH_PROFILE_NAME,
+                                String=b'SYSTEM.DEFAULT.MODEL.QUEUE'))
+        attrs.append(pymqi.CFIN(Parameter=pymqi.CMQCFC.MQIACF_OBJECT_TYPE,
+                                Value=pymqi.CMQC.MQOT_Q))
+        attrs.append(pymqi.CFIL(Parameter=pymqi.CMQCFC.MQIACF_AUTH_ADD_AUTHS,
+                                Values=[pymqi.CMQCFC.MQAUTH_DISPLAY,
+                                        pymqi.CMQCFC.MQAUTH_INPUT]))
+        attrs.append(pymqi.CFSL(Parameter=pymqi.CMQCFC.MQCACF_PRINCIPAL_ENTITY_NAMES,
+                                Strings=[utils.py3str2bytes(cls.app_user)]))
+        cls.pcf.MQCMD_SET_AUTH_REC(attrs)
+
+        attrs = []  # type: List[pymqi.MQOpts]
+        attrs.append(pymqi.CFST(Parameter=pymqi.CMQCFC.MQCACF_AUTH_PROFILE_NAME,
+                                String=b'SYSTEM.ADMIN.COMMAND.QUEUE'))
+        attrs.append(pymqi.CFIN(Parameter=pymqi.CMQCFC.MQIACF_OBJECT_TYPE,
+                                Value=pymqi.CMQC.MQOT_Q))
+        attrs.append(pymqi.CFIL(Parameter=pymqi.CMQCFC.MQIACF_AUTH_ADD_AUTHS,
+                                Values=[pymqi.CMQCFC.MQAUTH_OUTPUT]))
+        attrs.append(pymqi.CFSL(Parameter=pymqi.CMQCFC.MQCACF_PRINCIPAL_ENTITY_NAMES,
+                                Strings=[utils.py3str2bytes(cls.app_user)]))
+        cls.pcf.MQCMD_SET_AUTH_REC(attrs)
+
+        attrs = []  # type: List[pymqi.MQOpts]
+        attrs.append(pymqi.CFST(Parameter=pymqi.CMQCFC.MQCACF_AUTH_PROFILE_NAME,
+                                String=utils.py3str2bytes(cls.queue_manager)))
+        attrs.append(pymqi.CFIN(Parameter=pymqi.CMQCFC.MQIACF_OBJECT_TYPE,
+                                Value=pymqi.CMQC.MQOT_Q_MGR))
+        attrs.append(pymqi.CFIL(Parameter=pymqi.CMQCFC.MQIACF_AUTH_ADD_AUTHS,
+                                Values=[pymqi.CMQCFC.MQAUTH_DISPLAY]))
+        attrs.append(pymqi.CFSL(Parameter=pymqi.CMQCFC.MQCACF_PRINCIPAL_ENTITY_NAMES,
+                                Strings=[utils.py3str2bytes(cls.app_user)]))
+        results = cls.pcf.MQCMD_SET_AUTH_REC(attrs)
+
+        if cls.pcf.is_connected:
+            cls.pcf.disconnect()
+        if cls.qmgr.is_connected:
+            cls.qmgr.disconnect()
+
+    def test_init_none(self):
+        qmgr = pymqi.QueueManager(None)
+        self.assertFalse(qmgr.is_connected)
+
+    @utils.with_env_complement('MQSERVER', config.MQ.APP_MQSERVER)
+    def test_init_name(self):
+        # As the connect method provides no way to supply user & password, this
+        # cannot work if the queue manager requires it
+        if self.CHCKCLNT == pymqi.CMQCFC.MQCHK_REQUIRED:
+            self.skipTest('Test not viable for user/password-requiring queue manager')
+            return
+
+        # connecting with queue manager name needs MQSERVER set properly
+        qmgr = pymqi.QueueManager(self.queue_manager)
+        self.assertTrue(qmgr.is_connected)
+
+        if qmgr.is_connected:
+            qmgr.disconnect()
+
+    @utils.with_env_complement('MQSERVER', config.MQ.APP_MQSERVER)
+    def test_connect(self):
+        # As the connect method provides no way to supply user & password, this
+        # cannot work if the queue manager requires it
+        if self.CHCKCLNT == pymqi.CMQCFC.MQCHK_REQUIRED:
+            self.skipTest('Test not viable for user/password-requiring queue manager')
+            return
+
+        qmgr = pymqi.QueueManager(None)
+        self.assertFalse(qmgr.is_connected)
+        qmgr.connect(self.queue_manager)
+        self.assertTrue(qmgr.is_connected)
+        if qmgr.is_connected:
+            qmgr.disconnect()
+
+    def test_connect_tcp_client(self):
+        qmgr = pymqi.QueueManager(None)
+        qmgr.connect_tcp_client(
+            self.queue_manager, pymqi.cd(), self.channel, self.conn_info, user=self.user,
+            password=self.password)
+        self.assertTrue(qmgr.is_connected)
+        if qmgr.is_connected:
+            qmgr.disconnect()
+
+    def test_connect_tcp_client_without_cred(self):
+        if self.CHCKCLNT == pymqi.CMQCFC.MQCHK_REQUIRED:
+            self.skipTest('Test not viable for user/password-requiring queue manager')
+            return
+
+        qmgr = pymqi.QueueManager(None)
+        with self.assertRaises(pymqi.MQMIError) as ex_ctx:
+            qmgr.connect_tcp_client(
+                self.queue_manager, pymqi.cd(), self.channel, self.conn_info)
+            self.assertEqual(ex_ctx.exception.reason, pymqi.CMQC.MQRC_NOT_AUTHORIZED)
+        if qmgr.is_connected:
+            qmgr.disconnect()
+
+    # This test should be run last
+    # ConnectionName list with unaccessible QM affects on channel name of the next test if MQSERVER used
+    # changing the order of ConnectionName entries does not affect to issue occurrence
+    def test_zzz_connect_tcp_client_conection_list(self):
+        qmgr = pymqi.QueueManager(None)
+        conn_info = '127.0.0.1(22),{0}'.format(self.conn_info)
+        qmgr.connect_tcp_client(
+            self.queue_manager, pymqi.cd(), self.channel, conn_info, user=self.user,
+            password=self.password)
+        self.assertTrue(qmgr.is_connected)
+        if qmgr.is_connected:
+            qmgr.disconnect()
+
+    # This test overlaps with
+    # test_mq80.test_successful_connect_without_optional_credentials,
+    # but hey, why not
+    def test_connect_tcp_client_with_none_credentials(self):
+        if self.CHCKCLNT == pymqi.CMQCFC.MQCHK_REQUIRED:
+            self.skipTest('Test not viable for user/password-requiring queue manager')
+            return
+
+        qmgr = pymqi.QueueManager(None)
+        qmgr.connect_tcp_client(
+            self.queue_manager, pymqi.cd(), self.app_channel, self.conn_info, user=None,
+            password=None)
+        self.assertTrue(qmgr.is_connected)
+        if qmgr.is_connected:
+            qmgr.disconnect()
+
+    def test_disconnect(self):
+        qmgr = pymqi.QueueManager(None)
+        qmgr.connect_tcp_client(
+            self.queue_manager, pymqi.cd(), self.channel, self.conn_info, user=self.user,
+            password=self.password)
+        self.assertTrue(qmgr.is_connected)
+        if qmgr.is_connected:
+            qmgr.disconnect()
+            self.assertFalse(qmgr.is_connected)
+
+    def test_get_handle_unconnected(self):
+        qmgr = pymqi.QueueManager(None)
+        self.assertRaises(pymqi.PYIFError, qmgr.get_handle)
+
+    def test_get_handle_connected(self):
+        qmgr = pymqi.QueueManager(None)
+        qmgr.connect_tcp_client(
+            self.queue_manager, pymqi.cd(), self.channel, self.conn_info, user=self.user,
+            password=self.password)
+        handle = qmgr.get_handle()
+        self.assertTrue(isinstance(handle, int))
+
+        if qmgr.is_connected:
+            qmgr.disconnect()
+
+    @unittest.skip('Not implemented yet')
+    def test_begin(self):
+        pass
+
+    @unittest.skip('Not implemented yet')
+    def test_commit(self):
+        pass
+
+    @unittest.skip('Not implemented yet')
+    def test_backout(self):
+        pass
+
+    def test_inquire(self):
+        qmgr = pymqi.QueueManager(None)
+        qmgr.connect_tcp_client(
+            self.queue_manager, pymqi.cd(), self.channel, self.conn_info, user=self.user,
+            password=self.password)
+        attribute = pymqi.CMQC.MQCA_Q_MGR_NAME
+        expected_value = utils.py3str2bytes(self.queue_manager)
+        attribute_value = qmgr.inquire(attribute)
+        self.assertEqual(len(attribute_value), pymqi.CMQC.MQ_Q_MGR_NAME_LENGTH)
+        self.assertEqual(attribute_value.strip(), expected_value)
+
+        if qmgr.is_connected:
+            qmgr.disconnect()
+            self.assertFalse(qmgr.is_connected)
+
+
+if __name__ == '__main__':
+    main(module="test_queue_manager")
---
+++ setup.py
@@ -0,0 +1,273 @@
+# Setup script usable for distutils.
+#
+# Original Author: Maas-Maarten Zeeman
+#
+# Linux build is re-entrant/multithreaded.
+
+# stdlib
+import os
+import sys
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+from distutils.core import Extension
+from distutils import spawn
+from struct import calcsize
+
+version = '1.12.8'
+
+# Build either in bindings or client mode.
+bindings_mode = 0
+if sys.argv[-1] in ('bindings', 'server'):
+    bindings_mode = 1
+    sys.argv = sys.argv[:-1]
+if sys.argv[-1] == 'client':
+    bindings_mode = 0
+    sys.argv = sys.argv[:-1]
+
+# Are we running 64bits?
+if calcsize('P') == 8:
+    bits = 64
+else:
+    bits = 32
+
+def get_windows_settings():
+    """ Windows settings.
+    """
+    if bits == 64:
+        library_dirs = [r'{}\tools\Lib64'.format(os.environ.get('MQ_FILE_PATH', '.')),
+                        r'c:\Program Files (x86)\IBM\WebSphere MQ\tools\Lib64']
+        include_dirs = [r'{}\tools\c\include'.format(os.environ.get('MQ_FILE_PATH', '.')),
+                        r'c:\Program Files (x86)\IBM\WebSphere MQ\tools\c\include']
+    else:
+        library_dirs = [r'{}\tools\Lib'.format(os.environ.get('MQ_FILE_PATH', '.')),
+                        r'c:\Program Files\IBM\WebSphere MQ\Tools\Lib']
+        include_dirs = [r'{}\tools\c\include'.format(os.environ.get('MQ_FILE_PATH', '.')),
+                        r'c:\Program Files\IBM\WebSphere MQ\tools\c\include']
+
+    if bindings_mode:
+        libraries = ['mqm']
+    else:
+        if bits == 64:
+            libraries = ['mqic']
+        else:
+            libraries = ['mqic32']
+
+    return library_dirs, include_dirs, libraries
+
+def get_sunos_zlinux_settings():
+    """ SunOS and z/Linux settings.
+    """
+    if bits == 64:
+        library_dirs = ['{}/lib64'.format(os.environ.get('MQ_FILE_PATH', '.')),
+                        '/opt/mqm/lib64']
+    else:
+        library_dirs = ['{}/lib'.format(os.environ.get('MQ_FILE_PATH', '.')),
+                        '/opt/mqm/lib']
+
+    include_dirs = ['{}/inc'.format(os.environ.get('MQ_FILE_PATH', '.')),
+                    '/opt/mqm/inc']
+
+    if bindings_mode:
+        libraries = ['mqm', 'mqmcs', 'mqmzse']
+    else:
+        libraries = ['mqic']
+
+    return library_dirs, include_dirs, libraries
+
+def get_aix_settings():
+    """ AIX settings.
+    """
+    if bits == 64:
+        library_dirs = ['{}/lib64'.format(os.environ.get('MQ_FILE_PATH', '.')),
+                        '/usr/mqm/lib64']
+    else:
+        library_dirs = ['{}/lib'.format(os.environ.get('MQ_FILE_PATH', '.')),
+                        '/usr/mqm/lib']
+
+    include_dirs = ['{}/inc'.format(os.environ.get('MQ_FILE_PATH', '.')),
+                    '/usr/mqm/inc']
+
+    if bindings_mode:
+        libraries = ['mqm_r']
+    else:
+        libraries = ['mqic_r']
+
+    return library_dirs, include_dirs, libraries
+
+def get_generic_unix_settings():
+    """ Generic UNIX, including Linux, settings.
+    """
+    if bits == 64:
+        library_dirs = ['{}/lib64'.format(os.environ.get('MQ_FILE_PATH', '.')),
+                        '/opt/mqm/lib64']
+    else:
+        library_dirs = ['{}/lib'.format(os.environ.get('MQ_FILE_PATH', '.')),
+                        '/opt/mqm/lib']
+
+    include_dirs = ['{}/inc'.format(os.environ.get('MQ_FILE_PATH', '.')),
+                    '/opt/mqm/inc']
+
+    if bindings_mode:
+        libraries = ['mqm_r']
+    else:
+        libraries = ['mqic_r']
+
+    return library_dirs, include_dirs, libraries
+
+def get_locations_by_command_path(command_path):
+    """ Extracts directory locations by the path to one of MQ commands, such as dspmqver.
+    """
+    command_dir = os.path.dirname(command_path)
+    mq_installation_path = os.path.abspath(os.path.join(command_dir, '..'))
+
+    if bits == 64:
+        library_dirs = ['{}/lib64'.format(mq_installation_path)]
+    else:
+        library_dirs = ['{}/lib'.format(mq_installation_path)]
+
+    include_dirs = ['{}/inc'.format(mq_installation_path)]
+
+    if bindings_mode:
+        libraries = ['mqm_r']
+    else:
+        libraries = ['mqic_r']
+
+    return library_dirs, include_dirs, libraries
+
+# Windows
+if sys.platform == 'win32':
+    library_dirs, include_dirs, libraries = get_windows_settings()
+
+# SunOS and z/Linux
+elif sys.platform == 'sunos5' or sys.platform == 'linux-s390':
+    library_dirs, include_dirs, libraries = get_sunos_zlinux_settings()
+
+# AIX
+elif sys.platform.startswith('aix'):
+    library_dirs, include_dirs, libraries = get_aix_settings()
+
+# At this point, to preserve backward-compatibility we try out generic
+# UNIX settings first, i.e. libraries and include files in well-known locations.
+# Otherwise, to support Mac, we look up dspmqver in $PATH.
+else:
+
+    has_generic_lib = os.path.exists('/opt/mqm/lib64') if bits == 64 else os.path.exists('/opt/mqm/lib')
+    has_mq_file_path = os.environ.get('MQ_FILE_PATH', False)
+
+    if has_generic_lib or has_mq_file_path:
+        library_dirs, include_dirs, libraries = get_generic_unix_settings()
+
+    else:
+
+        # On Mac, users can install MQ to any location so we look up
+        # the path that dspmqver is installed to and find the rest
+        # of the information needed in relation to that base directory.
+        dspmqver_path = spawn.find_executable('dspmqver')
+
+        # We have found the command so we will be able to extract the relevant directories now
+        if dspmqver_path:
+            library_dirs, include_dirs, libraries = get_locations_by_command_path(dspmqver_path)
+
+        else:
+            raise Exception('MQ libraries could not be found')
+
+if bindings_mode:
+    print('Building PyMQI bindings mode %sbits' % bits)
+else:
+    print('Building PyMQI client mode %sbits' % bits)
+
+print('Using library_dirs:`%s`, include:`%s`, libraries:`%s`' % (library_dirs, include_dirs, libraries))
+
+long_description = """
+Python library for IBM MQ
+-------------------------
+
+PyMQI is a production-ready, open-source Python extension for IBM MQ (formerly known as WebSphere MQ and MQSeries).
+
+For 20+ years, the library has been used by thousands of companies around the world with their queue managers running on
+Linux, Windows, UNIX and z/OS.
+
+Sample code
+===========
+
+To put a message on a queue:
+
+.. code-block:: python
+
+    import pymqi
+
+    queue_manager = pymqi.connect('QM.1', 'SVRCONN.CHANNEL.1', '192.168.1.121(1434)')
+
+    q = pymqi.Queue(queue_manager, 'TESTQ.1')
+    q.put('Hello from Python!')
+
+To read the message back from the queue:
+
+.. code-block:: python
+
+    import pymqi
+
+    queue_manager = pymqi.connect('QM.1', 'SVRCONN.CHANNEL.1', '192.168.1.121(1434)')
+
+    q = pymqi.Queue(queue_manager, 'TESTQ.1')
+    msg = q.get()
+    print('Here is the message:', msg)
+
+The backbone of IBM MQ Python messaging
+=======================================
+
+PyMQI is a low-level library that requires one to know IBM MQ APIs well.
+
+It serves, however, as the basis for
+IBM MQ
+support
+in
+`Zato <https://zato.io>`_,
+which is an enterprise
+API platform and backend application server in Python that lets one connect to many technologies
+with little or no programming.
+
+This includes IBM MQ queue managers as well as the ability to seamlessly integrate with Java JMS systems.
+
+.. image:: https://zato.io/en/docs/3.2/gfx/api/screenshots/conn1.png
+
+.. image:: https://zato.io/en/docs/3.2/gfx/api/screenshots/mq.png
+
+Learn more
+==========
+
+Visit the `documentation <https://zato.io/en/docs/3.2/pymqi/index.html?gh>`_ for more information and usage examples.
+"""
+
+setup(name = 'pymqi',
+    version = version,
+    description = 'Python IBM MQI Extension for IBM MQ (formerly WebSphere MQ and MQSeries).',
+    long_description = long_description,
+    author='Zato Source s.r.o.',
+    author_email='pymqi@zato.io',
+    url='https://zato.io/pymqi',
+    download_url='https://zato.io/pymqi/install.html',
+    platforms='OS Independent',
+    package_dir = {'': 'code'},
+    packages = ['pymqi'],
+    license='Python Software Foundation License',
+    keywords=('pymqi IBM MQ WebSphere WMQ MQSeries IBM middleware messaging queueing asynchronous SOA EAI ESB integration'),
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: Python Software Foundation License',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: C',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Object Brokering',
+        ],
+    py_modules = ['pymqi.CMQC', 'pymqi.CMQCFC', 'pymqi.CMQXC', 'pymqi.CMQZC'],
+    ext_modules = [Extension('pymqi.pymqe',['code/pymqi/pymqe.c'], define_macros=[('PYQMI_BINDINGS_MODE_BUILD', bindings_mode)],
+        library_dirs = library_dirs,
+        include_dirs = include_dirs,
+        libraries = libraries)])
